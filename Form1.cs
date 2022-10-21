93a04cdb9137cf913d9cdfcea104ee8b013618ab
Mekaella Joy Muit <116326887+MJBayer@users.noreply.github.com>
2022-10-21 17:13:37 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Muit
+{
+    public partial class frmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+        public long StudentNumber(string studNum)
+        {
+            if (Regex.IsMatch(studNum, @"^[0-9]{1,15}$"))
+            {
+                _StudentNo = long.Parse(studNum);
+            }
+            else
+            {
+                throw new FormatException("The user must only enter a numeral data in the Student No. textbox.");
+            }
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+            else
+            {
+                throw new FormatException("The user must only enter numbers in the Contact No. textbox.");
+            }
+
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") &&  Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+            else
+            {
+                throw new FormatException("The user must only enter a character or alphabets in the Name's textbox.");
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+            else
+            {
+                throw new FormatException("The user must only enter a number in the Age textbox.");
+            }
+
+            return _Age;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]
+            {
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS in Tourism Management",
+            };
+            for(int i = 0; i < 6; i++)
+                {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+                }
+
+            string[] ListOfGender = new string[]
+            {
+                "Female",
+                "Male",
+                "LGBTQ"
+
+            };
+            for (int i = 0; i < 3; i++)
+            {
+                cbGender.Items.Add(ListOfGender[i].ToString());
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+                StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+                StudentInformationClass.SetProgram = cbPrograms.Text;
+                StudentInformationClass.SetGender = cbGender.Text;
+                StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+                StudentInformationClass.SetAge = Age(txtAge.Text);
+                StudentInformationClass.SetBirthDay = datePickerBirthday.Value.ToString("yyyyMM-dd");
+
+                frmConfirmation frm = new frmConfirmation();
+                frm.ShowDialog();
+
+                    txtLastName.Clear();
+                    txtFirstName.Clear();
+                    txtMiddleInitial.Clear();
+                    txtStudentNo.Clear();
+                    txtContactNo.Clear();
+                    txtAge.Clear();
+
+                    datePickerBirthday.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+
+                    cbPrograms.SelectedIndex = -1;
+                    cbGender.SelectedIndex = -1;
+                
+            }
+            catch (FormatException x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+        private void cbGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
